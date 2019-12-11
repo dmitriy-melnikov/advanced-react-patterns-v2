a69b6f5b35707522d791663617ce28af81f6a70b
dzmitriy-melnikov <dimaira2012@yandex.ru>
2019-12-11 23:43:36 +0300

    exc 4 render props

--- src/exercises/04.js
+++ src/exercises/04.js
@@ -6,18 +6,23 @@ const Toggle = (props) => {
   useEffect(() => props.onToggle(on));
   const toggle = () => setOn(!on);
   return (
-  this.props.render({on, toggle})
+    //component injection
+    React.createElement(props.children, {
+			on, toggle
+    })
+    //props.children({on, toggle})
+  //props.render({on, toggle})
   )
-}
+};
 
 
-const OriginalToggle = (props) => {
+/*const OriginalToggle = (props) => {
   return (
     <Toggle {...props}>
       {({on, toggle}) => <Switch on={on} onClick={toggle} />}
     </Toggle>
   )
-};
+};*/
 
 const ToggleChild = ({on, toggle}) => (
   <div>
@@ -36,14 +41,34 @@ function Usage({
   return (
     <div>
       {/*<OriginalToggle onToggle={onToggle}/>*/}
-			{/*<Toggle onToggle={onToggle}>
-        {props => <ToggleChild {...props} />}
-    </Toggle>*/}
-      <Toggle onToggle={onToggle}  render={props => <ToggleChild {...props} />} />
+			<Toggle onToggle={onToggle}>
+        {ToggleChild}
+        {/*{props => <ToggleChild {...props} />}*/}
+    </Toggle>
+      {/*<Toggle onToggle={onToggle}  render={props => <ToggleChild {...props} />} />*/}
     </div>
 
   )
 }
+
+/*function Usage({
+								 onToggle = (...args) => console.log('onToggle', ...args),
+							 }) {
+	return (
+    <Toggle onToggle={onToggle}>
+			{({on, toggle}) => (
+        <div>
+					{on ? 'The button is on' : 'The button is off'}
+          <Switch on={on} onClick={toggle} />
+          <hr />
+          <button aria-label="custom-button" onClick={toggle}>
+						{on ? 'on' : 'off'}
+          </button>
+        </div>
+			)}
+    </Toggle>
+	)
+}*/
 Usage.title = 'Render Props';
 
 export {Toggle, Usage as default}
