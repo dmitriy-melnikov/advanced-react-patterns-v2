28d2503f1bebd577de2090f03266101f4dd45812
dzmitriy-melnikov <dimaira2012@yandex.ru>
2019-12-12 00:58:15 +0300

    exc 6 render props collection getter

--- src/exercises/06.js
+++ src/exercises/06.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import {Switch} from '../switch'
+import React, { useState, useEffect } from 'react'
+import { Switch } from '../switch'
 
 
 // 💰 Here's a little utility that might come in handy
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args));
 
 
-class Toggle extends React.Component {
-  state = {on: false};
-  toggle = () =>
-    this.setState(
-      ({on}) => ({on: !on}),
-      () => this.props.onToggle(this.state.on),
-    );
+const Toggle = (props) => {
+  const [on, setOn] = useState(false);
+  useEffect(() => props.onToggle(on));
+  const toggle = () => setOn(!on);
 
-	getTogglerProps = ({onClick, ...props}) => ({
-		  onClick: callAll(onClick, this.toggle),
-			'aria-pressed': this.state.on,
+	const getTogglerProps = ({onClick, ...props}) => ({
+		  onClick: callAll(onClick, toggle),
+			'aria-pressed': on,
       ...props
 		});
 
-  getStateAndHelpers() {
+  const getStateAndHelpers = () => {
     return {
-      on: this.state.on,
-      toggle: this.toggle,
-      getTogglerProps: this.getTogglerProps
+      on,
+      toggle,
+      getTogglerProps
     }
-  }
+  };
 
+  return props.children(getStateAndHelpers())
 
-  render() {
-    return this.props.children(this.getStateAndHelpers())
-  }
-}
+};
 
 
 function Usage({
